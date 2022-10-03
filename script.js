9325a1b174ca8d0dd334a9263fd119947c3cf9ec
gregorec <gregorec.veronika@gmail.com>
2022-10-03 18:53:59 +0200

    Calculator

---
+++ script.js
@@ -0,0 +1,29 @@
+const result = document.getElementById("result");
+const buttons = Array.from(document.getElementsByClassName("button"));
+
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerHTML) {
+      case "C":
+        result.innerHTML = "";
+        break;
+      case "π":
+        result.innerHTML += 3.14;
+        break;
+      case "=":
+        try {
+          result.innerHTML = eval(result.innerHTML);
+        } catch {
+          result.innerHTML = "Error";
+        }
+        break;
+      case "←":
+        if (result.innerHTML) {
+          result.innerHTML = result.innerHTML.slice(0, -1);
+        }
+        break;
+      default:
+        result.innerHTML += e.target.innerHTML;
+    }
+  });
+});
